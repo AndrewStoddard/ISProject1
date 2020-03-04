28fe182f4469e8d80e0ed9cf82a4df4312b1935e
Justin Cintron <jcintro1@my.westga.edu>
2020-03-04 14:13:24 -0500

    Added adjustValue method's implementation.

--- ISProject1/Project1GroupH/src/edu/westga/cs3270/model/Agent.java
+++ ISProject1/Project1GroupH/src/edu/westga/cs3270/model/Agent.java
@@ -11,19 +11,22 @@ import java.util.Map;
  *
  */
 public class Agent {
-	private Map<State, Map<Action, Integer>> qTable;
+	private Map<State, Map<Action, Double>> qTable;
 	
 	public Agent() { 
-		this.qTable = new HashMap<State, Map<Action, Integer>>();
+		this.qTable = new HashMap<State, Map<Action, Double>>();
 	}
 	
 	public void initializeQTable() {
 		//Add every state to hash map. For each one, make the map with each action that a state can perform and set the value to 0
 	}
 	
-	public void adjustValue(State state) {
+	public void adjustValue(State state, Action action, double value) {
 		//When the agent moves to a new state, the previous state (passed in) and the action taken will update that integer value.
 		//i.e. If you move north, from the state (1,1) to  the state (1,2) then in the map, (1,1) - North - [new value]
+		Map<Action, Double> newMap = new HashMap<Action, Double>();
+		newMap.put(action, value);
+		this.qTable.replace(state, newMap);
 	}
 	
 }

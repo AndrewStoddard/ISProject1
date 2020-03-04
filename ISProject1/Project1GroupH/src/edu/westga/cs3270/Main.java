6e17a315a1bf68177c3331bb54faac5091062de4
Justin Cintron <jcintro1@my.westga.edu>
2020-03-04 13:46:13 -0500

    Added ability to change hyperparameters

--- ISProject1/Project1GroupH/src/edu/westga/cs3270/Main.java
+++ ISProject1/Project1GroupH/src/edu/westga/cs3270/Main.java
@@ -7,7 +7,14 @@ import edu.westga.cs3270.model.*;
  *
  */
 public class Main {
+	
+	private static final int RUN_MODE = 1;
+	public static double epsilon = 0.0;
+	public static double gamma = 0.0;
+	public static double alpha = 0.0;
+	
     public static void main(String[] args) {
+    	setHyperParameters();
         String input = "8x4" + System.lineSeparator()
         + "0,0,-100,0,0,0,0,0" + System.lineSeparator()
         + "0,0,0,0,0,0,-150,0" + System.lineSeparator()
@@ -15,17 +22,44 @@ public class Main {
         + "Start,-100,-100,0,-100,-100,-100,LOI+100";
         var inputParser = new InputParser(input);
 
-        System.out.println("Dim: " + inputParser.getWidth() + " x " + inputParser.getHeight());
+        System.out.println("Dim: " + inputParser.getMapWidth() + " x " + inputParser.getMapHeight());
         System.out.println("Start: " + inputParser.getStartPoint().getX() + ", " + inputParser.getStartPoint().getY());
         System.out.println("LOIs: " + inputParser.getLocationsOfIntrest().get(0).getX() + ", " + inputParser.getLocationsOfIntrest().get(0).getY());
         System.out.println("Start Val: " + inputParser.getMap()[0][0]);
        
         int[][] table = inputParser.getMap();
-        for (int i = inputParser.getHeight() - 1; i >= 0; i--) {
-        	for (int j = 0; j < inputParser.getWidth(); j++) {
+        for (int i = inputParser.getMapHeight() - 1; i >= 0; i--) {
+        	for (int j = 0; j < inputParser.getMapWidth(); j++) {
         		System.out.print(table[i][j] + " ");
         	}
         	System.out.println("");
         }
+        
+        
+    }
+    
+    private static void setHyperParameters() {
+    	switch (RUN_MODE) {
+    	case 1:
+    		epsilon = 0.1;
+    		gamma = 0.9;
+    		alpha = 0.5;
+    		break;
+    	case 2:
+    		epsilon = 1.0;
+    		gamma = 0.9;
+    		alpha = 0.5;
+    		break;
+    	case 3:
+    		epsilon = 0.0;
+    		gamma = 0.9;
+    		alpha = 0.5;
+    		break;
+    	case 4:
+    		epsilon = 0.1;
+    		gamma = 0.2;
+    		alpha = 0.7;
+    		break;
+    	}
     }
 }
